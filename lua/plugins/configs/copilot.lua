58af593d26209c931bc32ab86be08bfffeb6e090
Morten Olsen <fbtijfdq@void.black>
2022-11-10 15:42:12 +0100

    update

--- lua/plugins/configs/copilot.lua
+++ lua/plugins/configs/copilot.lua
@@ -1,5 +1,5 @@
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-x>", 'copilot#Accept("")', {script = true, silent = true, expr = true })
 
 vim.g.copilot_filetypes = {
   ["*"] = false,
